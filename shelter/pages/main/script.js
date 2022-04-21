7e62c052ec922a7ad45d84c467190e1c9f425d52
Kiryl Knyazev <kiril.kn99@email.ru>
2022-04-22 00:32:22 +0300

    feat: update burger menu in main page

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,7 +1,20 @@
-document.querySelector('.burger').addEventListener('click', () => {
+// BURGER MENU
+
+const divLayer = document.createElement('div')
+document.body.append(divLayer)
+
+function openMenu(){
     document.querySelector('.burger').classList.toggle('open-menu')
     document.querySelector('nav').classList.toggle('open-menu')
     document.querySelector('body').classList.toggle('open-menu')
     document.querySelector('.logo').classList.toggle('open-menu')
-    
+    divLayer.classList.toggle('layer-open-menu')
+}
+
+document.querySelector('.burger').addEventListener('click', openMenu)
+
+document.querySelectorAll('.nav-link').forEach(elem => {
+    elem.addEventListener('click',openMenu)
 })
+
+divLayer.addEventListener('click', openMenu)
