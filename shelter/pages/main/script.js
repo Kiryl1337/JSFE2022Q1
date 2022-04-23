ebd647f58b944a062e6a659f4e58b1ec94ef82eb
Kiryl Knyazev <kiril.kn99@email.ru>
2022-04-23 23:18:10 +0300

    feat: add popup in main page

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,3 +1,5 @@
+import petsArr from './pets.js';
+
 // BURGER MENU
 
 const divLayer = document.createElement('div')
@@ -18,3 +20,43 @@ document.querySelectorAll('.nav-link').forEach(elem => {
 })
 
 divLayer.addEventListener('click', openMenu)
+
+// POPUP
+
+const divBlackout = document.createElement('div')
+document.body.append(divBlackout)
+
+
+function openModal(event) {
+  document.querySelector('body').classList.toggle('open-modal')
+  document.querySelector('.modal-window-wrapper').style.display = 'block';
+  divBlackout.classList.add('blackout');
+  const petName = event.currentTarget.querySelector('section.pets .container .pets-slider .pets-slider-items .pets-slider-item h3').innerHTML;
+  const petInfo = petsArr.find((_, i, arr) => {
+    if (arr[i]["name"] === petName) {
+      return arr[i];
+    }
+  });
+
+  document.querySelector('.modal-window-container img').src = petInfo["img"].toString();
+  document.querySelector('.pet-name').textContent = petInfo["name"].toString();
+  document.querySelector('.pet-type').textContent = petInfo["type"].toString() + ' - ' + petInfo["breed"].toString();
+  document.querySelector('.pet-description').textContent = petInfo["description"].toString();
+  document.querySelector('.value.age').textContent = petInfo["age"].toString();
+  document.querySelector('.value.inoculations').textContent = petInfo["inoculations"].toString();
+  document.querySelector('.value.diseases').textContent = petInfo["diseases"].toString();
+  document.querySelector('.value.parasites').textContent = petInfo["parasites"].toString();
+}
+
+function closeModal() {
+  document.querySelector('body').classList.toggle('open-modal')
+  document.querySelector('.modal-window-wrapper').style.display = 'none';
+  divBlackout.classList.remove('blackout')
+}
+
+document.querySelectorAll('.pets-slider-item').forEach((el) => { 
+    el.addEventListener('click', openModal) 
+}); 
+
+divBlackout.addEventListener('click', closeModal);
+document.querySelector('.btn-modal-close').addEventListener('click', closeModal);
