dd4fa0cdeda44ae91a38b7145828820eabd81daa
Kiryl Knyazev <kiril.kn99@email.ru>
2022-04-25 01:52:22 +0300

    feat: add carousel with pet cards in main page

--- shelter/pages/script.js
+++ shelter/pages/script.js
@@ -61,3 +61,41 @@ document.querySelectorAll('.pet-card-item').forEach((el) => {
 
 divBlackout.addEventListener('click', closeModal);
 document.querySelector('.btn-modal-close').addEventListener('click', closeModal);
+
+// SLIDER
+
+window.load = slider();
+document.querySelector('.btn-arrow:nth-child(1)').addEventListener('click', slider);
+document.querySelector('.btn-arrow:last-child').addEventListener('click', slider);
+
+function slider(){
+
+  let itemsCount = window.innerWidth >= 1280 ? 3 : (window.innerWidth < 1280 && window.innerWidth >= 768) ? 2 : 1;
+  let petsOnPage = [];
+  for (let i = 0; i < itemsCount; i++) {
+    let elem = document.querySelectorAll('.pet-card-item h3')[i]
+    petsOnPage.push(elem.innerHTML);
+  }
+
+  let notPetsOnPage = [];
+  for (let i = 0; i < petsArr.length; i++) {
+    if(petsOnPage[0] !== petsArr[i]['name'] && petsOnPage[1] !== petsArr[i]['name'] && petsOnPage[2] !== petsArr[i]['name']){
+      notPetsOnPage.push(petsArr[i]);
+    }
+  }
+
+  let randomPets = []
+  for (let i = 0; randomPets.length < 3; i++) {
+    const randomPet = notPetsOnPage[Math.floor(Math.random() * notPetsOnPage.length)];
+    if(!randomPets.includes(randomPet)){
+      randomPets.push(randomPet);
+    }
+  }
+
+  for (let i = 0; i < 3; i++) {
+    let currentItem = document.querySelectorAll('.pet-card-item')[i];
+    currentItem.querySelector('.pet-img img').src = randomPets[i]['img'].toString();
+    currentItem.querySelector('h3').textContent = randomPets[i]['name'].toString();
+  }
+  
+}
